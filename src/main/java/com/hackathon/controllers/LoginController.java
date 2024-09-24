6e6daf389c14cf93302fd6805a126bf02af2a50e
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-09-23 22:38:22 -0300

    Security Config implemented checkpoint

---
+++ src/main/java/com/hackathon/controllers/LoginController.java
@@ -0,0 +1,30 @@
+package com.hackathon.controllers;
+
+import com.hackathon.entities.Doctor;
+import com.hackathon.entities.dtos.DoctorRequestDto;
+import com.hackathon.entities.dtos.LoginDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+public class LoginController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    ResponseEntity<String> login(@RequestBody LoginDto loginDto) throws Exception {
+        var token = new UsernamePasswordAuthenticationToken(loginDto.login(), loginDto.password());
+        var authentication = authenticationManager.authenticate(token);
+
+        return new ResponseEntity<>("Login efetuado com sucesso", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/hackathon/entities/User.java
@@ -0,0 +1,63 @@
+package com.hackathon.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.hackathon.entities.dtos.PatientRequestDto;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.security.SecureRandom;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String login;
+    private String password;
+
+    public User(){}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
